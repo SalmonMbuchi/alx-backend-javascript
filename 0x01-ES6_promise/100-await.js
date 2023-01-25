49ee6b860c18272f11e813ccf63f341a147e41ff
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-01-25 23:07:13 +0300

    async correction

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,17 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  try {
-    const photo = await uploadPhoto();
-    const user = await createUser();
-    const obj = {
-      photo,
-      user,
-    };
+  const photo = await uploadPhoto();
+  const user = await createUser();
+  const obj = {
+    photo,
+    user,
+  };
 
-    return obj;
-    // return Promise.all([photo, user]).then((val) => console.log(val));
-  } catch (err) {
-    return (err);
-  }
+  return obj;
 }
