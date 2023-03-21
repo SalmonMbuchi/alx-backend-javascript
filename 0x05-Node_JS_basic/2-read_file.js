4f40994877734ab92ac75ba72395f2b84d8cb6db
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-03-21 13:51:41 +0300

    read a file synchronously

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,15 +1,30 @@
 const fs = require('fs');
 
-function countStudents(path) {
-  const stream = fs.createReadStream(path, 'utf-8');
+function countStudents(dbPath) {
+  try {
+    let students = fs.readFileSync(dbPath, 'utf-8');
+    students = students.split('\n');
+    students = students.slice(1, students.length - 1);
+    const courses = new Map();
 
-  stream.on('error', (error) => {
-    console.log('Cannot load the database');
-  });
+    students.forEach((student) => {
+      const studentData = student.split(',');
+      const firstName = studentData[0];
+      const field = studentData[3];
+      if (courses.has(field)) {
+        courses.get(field).students.push(firstName);
+        courses.get(field).count += 1;
+      } else {
+        courses.set(field, { students: [firstName], count: 1 });
+      }
+    });
 
-  stream.on('data', (data) => {
-    console.log('Number of students: ');
-  });
+    console.log(`Number of students: ${students.length}`);
+    courses.forEach((courseData, course) => {
+      console.log(`Number of students in ${course}: ${courseData.count}. List: ${courseData.students.join(', ')}`);
+    });
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 }
-
 module.exports = countStudents;
