38c7747b063fcfab4c976d61d623dadf512c1226
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-03-23 21:14:06 +0300

    unittest api

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -9,3 +9,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`API available on localhost port ${port}`);
 });
+module.exports = app;
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,28 +1,32 @@
 const chai = require('chai');
 const expect = chai.expect;
 const request = require('request');
-const app = require('./api.js');
 
 
 describe('Index page', function () {
-  it('should return OK status', function () {
-    return request.get('http://localhost:7865/')
-      .on('response', function(response) {
-        expect(response.statusCode).to.equal(200);
-      })
+  const params = {
+    url: 'http://localhost/7865/',
+    method: 'GET'
+  } 
+  
+  it('should return OK status', function (done) {
+    request(params, function(err, res, body) {
+      expect(res.statusCode).to.equal(200);
+      done();
+    })
   });
 
-  it('should have text as the content type', function () {
-    return request.get('http://localhost:7865/')
-      .on('response', function (response) {
-        expect(response.headers['content-type']).to.equal('text/html; charset=utf-8');
-      })
+  it('should have text as the content type', function (done) {
+    request(params, function (err, res, body) {
+      expect(res.headers['content-type']).to.equal('text/html; charset=utf-8');
+      done();
+    })
   });
 
-  it.skip('should return a message', function () {
-    return request.get('http://localhost:7865/')
-      .on('response', function (response) {
-        expect(response.body).to.equal('Welcome to the payment system');
-      })
+  it('should return a message', function (done) {
+    request(params, function(err, res, body) {
+      expect(body).to.contain('Welcome to the payment system');
+      done();
+    })
   })
 })
