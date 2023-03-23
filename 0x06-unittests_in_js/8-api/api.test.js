adb8e453fb4f6b5eda50e7ce6a5dd5f66ea2416e
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-03-23 21:29:00 +0300

    integrate tests

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 const request = require('request');
+const {describe, it} = require('mocha');
 
 
 describe('Index page', function () {
@@ -13,20 +14,20 @@ describe('Index page', function () {
     request(params, function(err, res, body) {
       expect(res.statusCode).to.equal(200);
       done();
-    })
+    });
   });
 
   it('should have text as the content type', function (done) {
     request(params, function (err, res, body) {
       expect(res.headers['content-type']).to.equal('text/html; charset=utf-8');
       done();
-    })
+    });
   });
 
   it('should return a message', function (done) {
     request(params, function(err, res, body) {
       expect(body).to.contain('Welcome to the payment system');
       done();
-    })
-  })
-})
+    });
+  });
+});
