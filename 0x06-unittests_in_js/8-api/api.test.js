0ccfbc434e7a93960be89a41ca559966d48c6f54
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-03-24 22:22:06 +0300

    test Express API

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -9,7 +9,7 @@ describe('Index page', function () {
     request.get('http:localhost:7865/', function(err, res, body) {
       if (!err) {
         expect(res.statusCode).to.equal(200);
-        expect(body).to.contain('Welcome to the payment system');
+        expect(body).to.equal('Welcome to the payment system');
         expect(res.headers['content-type']).to.equal('text/html; charset=utf-8');
       }
       done();
