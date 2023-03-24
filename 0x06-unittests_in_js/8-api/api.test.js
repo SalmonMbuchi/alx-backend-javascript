7286a03b04c09f1c42d0ad4434b49d648c3c6ae8
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-03-24 14:53:43 +0300

    rewrite tests

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -5,19 +5,8 @@ const {describe, it} = require('mocha');
 
 
 describe('Index page', function () {
-  const params = {
-    url: 'http://localhost/7865/',
-    method: 'GET'
-  } 
-  
-  it('tests that GET / exists', function (done) {
-    request.get('https://localhost:7865', function (err, res) {
-      if (!err) expect (res.statusCode).to.equal(200);
-    })
-    done();
-  })
-  it('tests the Express API', function (done) {
-    request(params, function(err, res, body) {
+  it('should return OK status', function (done) {
+    request.get('http:localhost:7865/', function(err, res, body) {
       if (!err) {
         expect(res.statusCode).to.equal(200);
         expect(body).to.contain('Welcome to the payment system');
