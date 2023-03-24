dd9328f521cd4bd60eca8cf836ae29afbc780a94
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-03-24 21:26:40 +0300

    use done callback on async tests

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,13 +1,11 @@
 const chai = require('chai');
 const expect = chai.expect;
 const { describe, it } = require('mocha');
-const getpaymentTokenFromAPI = require('./6-payment_token.js');
-
-
+const getPaymentTokenFromAPI = require('./6-payment_token.js');
 
 describe('getpaymentTokenFromAPI', function () {
   it('should return an object if true', function (done) {
-    getpaymentTokenFromAPI(true)
+    getPaymentTokenFromAPI(true)
       .then((res) => {
         expect(res).to.be.an('object')
           .with.property('data', 'Successful response from the API');
