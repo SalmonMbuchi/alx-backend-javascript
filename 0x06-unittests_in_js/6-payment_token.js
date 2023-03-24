86683101d4b7b8c60d644cd92690df8ee942c915
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-03-24 16:54:38 +0300

    payment token

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,9 +1,8 @@
 module.exports = function getPaymentTokenFromAPI(success) {
   if (success === 'true') {
-    //return new Promise((resolve, reject) => {
-    //  const obj = {data: 'Succesful response from the API'};
-    //  resolve(obj);
-    //})
-    return Promise.resolve({data: 'Succesful response from API'});
+    return new Promise((resolve) => {
+      const obj = {data: 'Succesful response from the API'};
+      resolve(obj);
+    })
   }
 }
--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,15 +1,17 @@
 const chai = require('chai');
 const expect = chai.expect;
+const { describe, it } = require('mocha');
 const getpaymentTokenFromAPI = require('./6-payment_token.js');
 
 
 
 describe('getpaymentTokenFromAPI', function () {
   it('should return an object if true', function (done) {
-    return getpaymentTokenFromAPI(true).
-      then(obj => {
-        expect(obj).to.equal({data: 'Successfully response from the API'});
+    getpaymentTokenFromAPI(true).
+      then((res) => {
+        expect(res).to.be.an('object')
+          .with.property('data', 'Successful response from the API');
         done();
-      });
-  })
-})
+      })
+  });
+});
